64bcb5b396acc7efb162306421eb12f908dca765
lflyl <2485741103@qq.com>
2020-02-25 00:20:05 +0800

    first commit

---
+++ app/src/main/java/mg/studio/android/survey/MainActivity.java
@@ -0,0 +1,16 @@
+package mg.studio.android.survey;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.RadioButton;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcome);
+    }
+
+}
